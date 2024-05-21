195086e6eece305a737a383e0b1d4ee95b833853
Christopher Cook <96829867+7NEO1990@users.noreply.github.com>
2024-05-21 14:20:47 -0500

    Create sentence_to_list.py

---
+++ sentence_to_list.py
@@ -0,0 +1,5 @@
+sentence = input("Enter a sentence: ")#asks user to enter a sentence
+sentence_list = sentence.split()#splits the sentence into a list
+print(sentence_list)
+reversed_sentence = sentence_list[::-1]#reverses the list
+print(reversed_sentence)
